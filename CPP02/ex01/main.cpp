576ba68cee5011fdd7822e19a35ef7fde433ec2e
Christian JIMENEZ <cjimenez@e2r9p7.clusters.42paris.fr>
2022-10-25 17:05:09 +0200

    estethique

--- CPP02/ex01/main.cpp
+++ CPP02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cjimenez <cjimenez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 16:04:12 by cjimenez          #+#    #+#             */
-/*   Updated: 2022/10/25 16:04:28 by cjimenez         ###   ########.fr       */
+/*   Updated: 2022/10/25 17:04:18 by cjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ int main( void )
     Fixed const c( 42.42f );
     Fixed const d( b );
     a = Fixed( 1234.4321f );
+    
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
     std::cout << "c is " << c << std::endl;
     std::cout << "d is " << d << std::endl;
+    
     std::cout << "a is " << a.toInt() << " as integer" << std::endl;
     std::cout << "b is " << b.toInt() << " as integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
