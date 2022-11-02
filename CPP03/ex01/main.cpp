51cf7ce2dcacab95f25188f8117a92fbb5c20643
Christian JIMENEZ <cjimenez@e2r8p12.clusters.42paris.fr>
2022-11-02 20:48:08 +0100

    rammus qui scale

--- CPP03/ex01/main.cpp
+++ CPP03/ex01/main.cpp
@@ -6,50 +6,23 @@
 /*   By: cjimenez <cjimenez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 17:25:16 by cjimenez          #+#    #+#             */
-/*   Updated: 2022/11/02 20:28:23 by cjimenez         ###   ########.fr       */
+/*   Updated: 2022/11/02 20:46:41 by cjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 
-int	main(void)
-{
-	std::string s1 = "Clyde";
-	std::string s2 = "Scarlet";
-
-	ClapTrap claptrap(s1);
-	ScavTrap scavtrap(s2);
-
-	std::cout << std::endl;
-	claptrap.attack(s2);
-	scavtrap.takeDamage(0);
-	scavtrap.beRepaired(18);
-	std::cout << std::endl;
-	scavtrap.attack(s1);
-	claptrap.takeDamage(20);
-	scavtrap.attack(s1);
-	claptrap.takeDamage(20);
-	scavtrap.attack(s1);
-	claptrap.beRepaired(64);
-	std::cout << std::endl;
-	scavtrap.guardGate();
-	scavtrap.attack(s1);
-	std::cout << std::endl;
-	scavtrap.takeDamage(100);
-	scavtrap.takeDamage(1);
-	scavtrap.attack(s2);
-	scavtrap.beRepaired(200);
-	
-	std::cout << std::endl;
-	return (0);
-}
-
-// int main (void){
-//     ScavTrap test("01");
+int main (void){
+    ScavTrap test("Rammus");
+    ScavTrap Yasuo("yasuo");
     
-//     test.attack("you");
-//     test.guardGate();
-//     test.beRepaired(100);
-//     return 0;
-// }
+    test.attack("you");
+    test.guardGate();
+    test.beRepaired(100);
+    
+    Yasuo.attack("you");
+    Yasuo.guardGate();
+    Yasuo.beRepaired(100);
+    return 0;
+}
